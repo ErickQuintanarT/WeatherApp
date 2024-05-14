ae7b5d12c80a09fd43d10eb115518c5e102e9470
Erick Quintanar <equintanart@gmail.com>
2024-05-14 14:06:26 -0600

    Change on Network Manager Protocol

---
+++ WeatherApp/Protocols/NetworkManagerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManagerProtocol.swift
+//  WeatherApp
+//
+//  Created by Erick Quintanar on 5/13/24.
+//
+
+import Foundation
+import UIKit
+
+protocol NetworkManagerProtocol {
+    func fetchWeatherData<T: Codable>(from url: URL, completion: @escaping (Result<T, Error>) -> Void)
+    func fetchImageData(from url: URL, completion: @escaping (Result<Data, Error>) -> Void)
+}
